9ab619aa040c87f6fe9df1ab3591201013911c5a
Ryder Bergerud <ryder.bergerud@gmail.com>
2017-02-23 19:20:36 -0800

    Added elimination, only_choice, and reduce_puzzle functions
    - Reduce_puzzle will be able to solve some easy sudokus.
    - eliminate will be able to eliminate values from peers of each box with a single value.
    - Finalize all values that are the only choice for a unit.

--- solution.py
+++ solution.py
@@ -91,13 +91,66 @@ def display(values):
     return
 
 def eliminate(values):
-    pass
+    """Eliminate values from peers of each box with a single value.
+
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+
+    Args:
+        values: Sudoku in dictionary form.
+    Returns:
+        Resulting Sudoku in dictionary form after eliminating values.
+    """
+    knowns = [ind for ind, box in values.items() if len(box) == 1]
+
+    for known in knowns:
+        for peer in peers[known]:
+            values[peer] = values[peer].replace(values[known], '')
+    return values
 
 def only_choice(values):
-    pass
+    """Finalize all values that are the only choice for a unit.
+
+    Go through all the units, and whenever there is a unit with a value
+    that only fits in one box, assign the value to this box.
+
+    Input: Sudoku in dictionary form.
+    Output: Resulting Sudoku in dictionary form after filling in only choices.
+    """
+    for unit in unitlist:
+        to_check = '123456789'
+        for number in to_check:
+            #does only one box have this number?
+            candidate_boxes = [box for box in unit if values[box].find(number) != -1]
+            if len(candidate_boxes) == 1:
+                values[candidate_boxes[0]] = number
+    return values
+
 
 def reduce_puzzle(values):
-    pass
+    """
+    Iterate eliminate() and only_choice(). If at some point, there is a box with no available values, return False.
+    If the sudoku is solved, return the sudoku.
+    If after an iteration of both functions, the sudoku remains the same, return the sudoku.
+    Input: A sudoku in dictionary form.
+    Output: The resulting sudoku in dictionary form.
+    """
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        # Use the Eliminate Strategy
+        values = eliminate(values)
+        # Use the Only Choice Strategy
+        values = only_choice(values)
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 def search(values):
     pass
